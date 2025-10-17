fe35f969c9db3ce364f845774c886405cce58e20
Euan Black <euanblack2002@gmail.com>
2025-10-17 17:33:02 +0100

    Finished

--- lib/age_checker.py
+++ lib/age_checker.py
@@ -6,17 +6,30 @@ from dateutil.relativedelta import relativedelta
 
 
 def age_checker(some_DOB):
+    if not isinstance(some_DOB, str):
+        raise TypeError('DOB must be a string in format YYYY-MM-DD')
+
+
     today = datetime.now()
 
     # date_format = '%Y-%m-%d'
-    DOB_datetime = parser().parse(some_DOB)
+    try:
+        DOB_datetime = parser().parse(some_DOB)
+    except ParserError:
+        raise ValueError('Invalid DOB format, must be YYYY-MM-DD')
+    
+    if DOB_datetime > today:
+        raise Exception('Cannot be date in the future')
+
     # diff_today_DOB = today - DOB_datetime
     # years_diff = (diff_today_DOB.days) / 365
     # return years_diff
     age = relativedelta(today, DOB_datetime)
 
+
     if age.years >= 16:
         return "access granted"
     return f"access denied, you are {age.years}, the required age is 16"
+    
 
     
--- tests/test_age_checker.py
+++ tests/test_age_checker.py
@@ -1,4 +1,5 @@
 from lib.age_checker import *
+import pytest
 
 def test_age_checker():
     # age_checker_today = datetime.strptime('1960-10-21')
@@ -11,7 +12,24 @@ def test_age_checker_also_too_young():
         assert age_checker('2015-01-22') == "access denied, you are 10, the required age is 16"
 
 def test_age_checker_birthday():
-     assert age_checker('2009-10-18') == "access denied, you are 15, the required age is 16"
+    assert age_checker('2009-10-18') == "access denied, you are 15, the required age is 16"
+
+def test_age_check_wrong_type():
+    with pytest.raises(Exception) as e:
+        age_checker(6)
+    error_message = str(e.value)
+    assert error_message == 'DOB must be a string in format YYYY-MM-DD'
+
+def test_age_checker_wrong_format():
+    with pytest.raises(ValueError) as e:
+        age_checker('hello')
+    error_message = str(e.value)
+    assert error_message == 'Invalid DOB format, must be YYYY-MM-DD'
 
 
+def test_age_checker_future():
+    with pytest.raises(Exception) as e:
+        age_checker('3000-10-10')
+    error_message = str(e.value)
+    assert error_message == 'Cannot be date in the future'
 
