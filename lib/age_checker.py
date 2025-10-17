93af6cf5954bc9075d4b90c4608c11997a4e87e2
Euan Black <euanblack2002@gmail.com>
2025-10-17 15:52:42 +0100

    End of turn 3

--- lib/age_checker.py
+++ lib/age_checker.py
@@ -1,4 +1,6 @@
-from datetime import datetime 
+from datetime import datetime
+import dateutil
+
 
 def age_checker(some_DOB):
     # return datetime.strptime('1960-10-21')
@@ -8,6 +10,6 @@ def age_checker(some_DOB):
     DOB_datetime = datetime.strptime(some_DOB, "%Y-%m-%d").date()
     diff_today_DOB = today - DOB_datetime
     years_diff = int((diff_today_DOB.days) / 365)
-    if years_diff > 16:
+    if years_diff >= 16:
         return "access granted"
-    return "access denied"
+    return f"access denied, you are {years_diff}, the required age is 16"
--- tests/test_age_checker.py
+++ tests/test_age_checker.py
@@ -5,4 +5,11 @@ def test_age_checker():
     assert age_checker('1960-10-21') == "access granted"
 
 def test_age_checker_too_young():
-    assert age_checker('2019-01-22') == "access denied"
+    assert age_checker('2019-01-22') == "access denied, you are 6, the required age is 16"
+
+def test_age_checker_also_too_young():
+        assert age_checker('2015-01-22') == "access denied, you are 10, the required age is 16"
+
+def test_age_checker_birthday():
+     assert age_checker('2009-10-18') == "access denied, you are 15, the required age is 16"
+
